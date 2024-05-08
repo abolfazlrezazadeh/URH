44ee6cb037dee4e09625a3c4f24fab96e9270a8f
Mahdi <mahdimey17@gmail.com>
2024-05-08 22:50:02 +0330

    add support page including tailwind styles

---
+++ view/src/Support.jsx
@@ -0,0 +1,29 @@
+import { BiMapAlt } from "react-icons/bi"; 
+import { BiSupport } from "react-icons/bi"; 
+import { BiTimer } from "react-icons/bi"; 
+import { BiBus } from "react-icons/bi"
+export default function Support() {
+  return (
+    <div>
+      <h1 className="mr-8 mt-24 text-3xl font-extrabold">
+        با چه مشکلی برخورد کرداید
+      </h1>
+      <div className="m-auto mt-8 flex w-4/5 cursor-pointer flex-row items-center gap-3 rounded-full border-b-2 border-gray-800 bg-slate-50 px-3 py-2">
+        <BiBus />
+        <button>گذارش تاخیر اتوبوس</button>
+      </div>
+      <div className="m-auto mt-8 flex w-4/5 cursor-pointer flex-row items-center gap-3 rounded-full border-b-2 border-gray-800 bg-slate-50 px-3 py-2">
+        <BiMapAlt />
+        <button>گزارش خطا در نقشه</button>
+      </div>
+      <div className="m-auto mt-8 flex w-4/5 cursor-pointer flex-row items-center gap-3 rounded-full border-b-2 border-gray-800 bg-slate-50 px-3 py-2">
+        <BiTimer />
+        <button>گزارش اشتباه در زمان اعلامی</button>
+      </div>
+      <div className="m-auto mt-8 flex w-4/5 cursor-pointer flex-row items-center gap-3 rounded-full border-b-2 border-gray-800 bg-slate-50 px-3 py-2">
+        <BiSupport />
+        <button>تماس با پشتیبان </button>
+      </div>
+    </div>
+  )
+}
