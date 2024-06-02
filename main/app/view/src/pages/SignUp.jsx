a08999a658ed39e30ad8a690a83c2dbf055c1c15
Mahdi <mahdimey17@gmail.com>
2024-06-02 18:25:17 +0330

    dispatch sendOtp action to userSlice

--- main/app/view/src/pages/SignUp.jsx
+++ main/app/view/src/pages/SignUp.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
-// import { useSelector } from "@reduxjs/toolkit"
+import { useDispatch, useSelector } from "react-redux"
 import VerificationInput from "react-verification-input"
+import { sendOtp } from "../features/user/userSlice"
 
 export default function SignUp() {
   const [phoneNumber, setPhoneNumber] = useState("")
-  // const signIn = useSelector(store => store.user.haveSms)
+  const dispatch = useDispatch()
+  // const userNumber = useSelector((store) => store.user.user.phoneNumber)
 
   const handlePhoneNumberChange = (e) => {
     let value = e.target.value
@@ -17,35 +19,35 @@ export default function SignUp() {
   function handleSubmit(e) {
     e.preventDefault()
     if (phoneNumber.length !== 11) return
-    console.log(phoneNumber)
+    dispatch(sendOtp(phoneNumber))
   }
 
   // if (signIn) {
-    return (
-      <>
-        <h1 className="mr-8 mt-24 text-4xl font-extrabold">خوش آمدید</h1>
-        <p className="mr-10 mt-10 text-base">لطفا شماره ی خودرا وارد فرمایید</p>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input
-            type="number"
-            className="m-auto mt-8 block w-4/5 rounded-2xl border border-gray-500 py-3 text-center placeholder-gray-600 placeholder:font-bold"
-            placeholder="شماره تلفن"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-            inputMode="numeric"
-            />
-          <p className="mr-10 mt-5 text-base">
-            با ثبت نام قوانین و شرایط را میپذیرم
-          </p>
-          <button
-            type="submit"
-            className="m-auto mt-5 block rounded-full bg-[#00FFC1] px-10 py-3"
-            >
-            دریافت کد
-          </button>
-        </form>
-      </>
-    )
+  return (
+    <>
+      <h1 className="mr-8 mt-24 text-4xl font-extrabold">خوش آمدید</h1>
+      <p className="mr-10 mt-10 text-base">لطفا شماره ی خودرا وارد فرمایید</p>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="number"
+          className="m-auto mt-8 block w-4/5 rounded-2xl border border-gray-500 py-3 text-center placeholder-gray-600 placeholder:font-bold"
+          placeholder="شماره تلفن"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+          inputMode="numeric"
+        />
+        <p className="mr-10 mt-5 text-base">
+          با ثبت نام قوانین و شرایط را میپذیرم
+        </p>
+        <button
+          type="submit"
+          className="m-auto mt-5 block rounded-full bg-[#00FFC1] px-10 py-3"
+        >
+          دریافت کد
+        </button>
+      </form>
+    </>
+  )
   // } else {
   //   return (
   //     <>
