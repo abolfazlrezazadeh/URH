4a809f5fd8f9fdd33a5b96486259b2a5ec77210c
Mahdi <mahdimey17@gmail.com>
2024-06-19 19:15:10 +0330

    add /bus Route to render Bus component

--- main/app/view/src/App.jsx
+++ main/app/view/src/App.jsx
@@ -1,16 +1,15 @@
-// ? pages
 import Information from "./pages/Information"
 import Invite from "./pages/Invite"
 import Settings from "./pages/Settings"
 import UserProfile from "./pages/UserProfile"
 import SignUp from "./pages/SignUp"
 import Support from "./pages/Support"
-import Confirm from "./Confirm"
 import GoBackBtn from "./components/GoBackBtn"
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Website from "./pages/Website"
 import Bus from "./pages/Bus"
+import Podcast from "./pages/Podcast"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
@@ -19,13 +18,15 @@ function App() {
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="website" element={<Website />} />
-          <Route path="confirm" element={<Confirm />} />
+
           <Route path="bus" element={<Bus />} />
+          <Route path="podcast" element={<Podcast />} />
           <Route path="information" element={<Information />} />
           <Route path="invite" element={<Invite />} />
           <Route path="settings" element={<Settings />} />
           <Route path="userprofile" element={<UserProfile />} />
           <Route path="support" element={<Support />} />
+
           <Route path="*" element={<GoBackBtn />} />
         </Routes>
       </BrowserRouter>
