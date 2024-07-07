fb4dd857c7d36b70372cd71c18f66d9e05bd3d72
Mahdi <mahdimey17@gmail.com>
2024-07-07 17:26:28 +0330

    add placeholder content and make the structure with styles

--- main/app/view/src/pages/Podcast.jsx
+++ main/app/view/src/pages/Podcast.jsx
@@ -2,45 +2,65 @@ import { CiSearch } from "react-icons/ci"
 
 export default function Podcast() {
   return (
-    <img
-      src="/assets/podcasd-croped.png"
-      alt="podcast-page"
-      className="h-[100svh] w-screen"
-      draggable='false'
-    />
-    // <div>
-    //   <h1>پادکست کده</h1>
+    <div>
+      <h1 className="font-bold text-2xl mt-5 mr-5">پادکست کده</h1>
 
-    //   <div>
-    //     <input type="text" placeholder="چی میخای گوش کنی؟ " />
-    //     <CiSearch />
-    //   </div>
+      <div>
+        <input
+          type="text"
+          placeholder="چی میخای گوش کنی؟ "
+          className="m-auto my-5 block w-4/5 bg-gray-200 placeholder:text-gray-800 placeholder:text-center rounded-md border border-gray-500 p-1"
+        />
+        {/* <CiSearch /> */}
+      </div>
 
-    //   <ItemWrapper>
-    //     <Header>علمی</Header>
-    //   </ItemWrapper>
+      <ItemWrapper>
+        <Header>علمی</Header>
+        <div className="flex flex-row gap-2">
+          <Item imgSrc="/assets/podcast/elmi1.png" title="زمین" />
+          <Item imgSrc="/assets/podcast/elmi2.png" title="مورچه" />
+          <Item imgSrc="/assets/podcast/elmi3.png" title="ماشین" />
+        </div>
+      </ItemWrapper>
 
-    //   <ItemWrapper>
-    //     <Header>مذهبی</Header>
-    //   </ItemWrapper>
+      <ItemWrapper>
+        <Header>مذهبی</Header>
+        <div className="flex flex-row gap-2">
+          <Item imgSrc="assets/podcast/mzhbi1.png" title="زیارت عاشورا" />
+          <Item imgSrc="assets/podcast/mzhbi2.png" title="انقلاب" />
+          <Item imgSrc="assets/podcast/mzhbi3.png" title="سخنان رهبر" />
+        </div>
+      </ItemWrapper>
 
-    //   <ItemWrapper>
-    //     <Header>توسعه فردی</Header>
-    //   </ItemWrapper>
-
-    // </div>
+      <ItemWrapper>
+        <Header>توسعه فردی</Header>
+        <div className="flex flex-row gap-2">
+          <Item imgSrc="assets/podcast/s1.png" title="هنر جنگ" />
+          <Item imgSrc="assets/podcast/s2.png" title="باشگاه 5 صبحی" />
+          <Item imgSrc="assets/podcast/s3.png" title="کون گشاد نباشیم" />
+        </div>
+      </ItemWrapper>
+    </div>
   )
 }
 
 function Header({ children }) {
-  return <h2 className="text-3xl border-t border-r border-gray-600 rounded-full w-max">{children}</h2>
+  return (
+    <h2 className="w-max rounded-full border-r border-t border-gray-600 text-3xl">
+      {children}
+    </h2>
+  )
 }
 
-function Item() {
-  return <>
-  </>
+function Item({ imgSrc, title }) {
+  return (
+    <di className='flex-col flex-center'>
+      <img src={imgSrc} alt={title} className="" />
+      <p className="">{title}</p>
+    </di>
+  )
 }
 
 function ItemWrapper({ children }) {
-  return <div className="flex flex-col">{children}</div>
+  return <div className="flex flex-col gap-3 mb-5">{children}</div>
 }
