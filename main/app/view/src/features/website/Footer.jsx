3b7dbe41372d69644da8bde9a437e6d9a995123d
Mahdi <mahdimey17@gmail.com>
2024-07-08 19:36:21 +0330

    fix color of icons to match website theme

--- main/app/view/src/features/website/Footer.jsx
+++ main/app/view/src/features/website/Footer.jsx
@@ -6,29 +6,29 @@ export default function Footer() {
   return (
     <>
       <div className="flex flex-wrap gap-2 p-2">
-        <div className="flex-center w-32 rounded-md border border-gray-600 p-4 text-center">
+        <div className="flex-center w-32 cursor-pointer rounded-md border border-gray-600 p-4 text-center">
           سوالات متداول
         </div>
-        <div className="flex-center w-32 rounded-md border border-gray-600 p-4 text-center">
-          شکایات{" "}
+        <div className="flex-center w-32 cursor-pointer rounded-md border border-gray-600 p-4 text-center">
+          شکایات
         </div>
-        <div className="flex-center w-32 rounded-md border border-gray-600 p-4 text-center">
+        <div className="flex-center w-32 cursor-pointer rounded-md border border-gray-600 p-4 text-center">
           تماس با ما
         </div>
-        <div className="flex-center w-32 rounded-md border border-gray-600 p-4 text-center">
-          درباره ی ما{" "}
+        <div className="flex-center w-32 cursor-pointer rounded-md border border-gray-600 p-4 text-center">
+          درباره ی ما
         </div>
       </div>
 
       {/* links to social media */}
       <div className="my-8 flex flex-row justify-center gap-5">
-        <BsInstagram className="text-3xl text-blue-600" />
-        <BsTelegram className="text-3xl text-blue-600" />
-        <BsTwitter className="text-3xl text-blue-600" />
-        <AiFillYoutube className="text-3xl text-blue-600" />
+        <BsInstagram className="cursor-pointer text-3xl text-[#5B7DF5]" />
+        <BsTelegram className="cursor-pointer text-3xl text-[#5B7DF5]" />
+        <BsTwitter className="cursor-pointer text-3xl text-[#5B7DF5]" />
+        <AiFillYoutube className="cursor-pointer text-3xl text-[#5B7DF5]" />
       </div>
 
-      <h1 className="text-left ml-2">Urh©2024</h1>
+      <h1 className="ml-2 text-left">Urh©2024</h1>
     </>
   )
 }
