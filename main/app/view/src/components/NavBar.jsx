ee644955237335ad8ff31672740ae76e3ce3a406
Mahdi <mahdimey17@gmail.com>
2024-07-08 19:56:39 +0330

    changed Link to NavLink, add active styling, changed order

--- main/app/view/src/components/NavBar.jsx
+++ main/app/view/src/components/NavBar.jsx
@@ -2,21 +2,66 @@ import { BiQr } from "react-icons/bi"
 import { FaTaxi } from "react-icons/fa"
 import { BsBusFrontFill } from "react-icons/bs"
 import { BiSupport } from "react-icons/bi"
+import { NavLink } from "react-router-dom"
 export default function NavBar() {
   return (
-    <nav className="absolute bottom-4 left-1/2 top-[90%] flex h-14 w-4/5 -translate-x-1/2 -translate-y-1/2 transform justify-around rounded-full border-t-2 border-gray-400">
-      <button className="text-2xl">
-        <FaTaxi />
-      </button>
-      <button className="text-2xl">
-        <BiQr />
-      </button>
-      <button className="text-2xl">
+    <nav className="absolute bottom-4 left-1/2 top-[90%] z-50 flex h-14 w-4/5 -translate-x-1/2 -translate-y-1/2 transform items-center justify-around rounded-full border-t-2 border-gray-400 bg-white/90">
+      <NavLink
+        to="/bus"
+        className={({ isActive }) => (isActive ? "bg-primary" : "")}
+        style={{
+          fontSize: "1.25rem",
+          padding: "0.75rem",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <BsBusFrontFill />
-      </button>
-      <button className="text-2xl">
+      </NavLink>
+      <NavLink
+        to="/qrcode"
+        className={({ isActive }) => (isActive ? "bg-primary" : "")}
+        style={{
+          fontSize: "1.25rem",
+          padding: "0.75rem",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <BiQr />
+      </NavLink>
+      <NavLink
+        to="/taxi"
+        className={({ isActive }) => (isActive ? "bg-primary" : "")}
+        style={{
+          fontSize: "1.25rem",
+          padding: "0.75rem",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <FaTaxi />
+      </NavLink>
+      <NavLink
+        to="/support"
+        style={{
+          fontSize: "1.25rem",
+          padding: "0.75rem",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        className={({ isActive }) => (isActive ? "bg-primary" : "")}
+      >
         <BiSupport />
-      </button>
+      </NavLink>
     </nav>
   )
 }
