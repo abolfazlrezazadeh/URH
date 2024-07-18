a837c6ec82734b087de49ae6ecb5902448d9195e
Mahdi <mahdimey17@gmail.com>
2024-07-18 15:43:45 +0330

    add two buttons and local state to toggle nfc and qrcode tab

--- main/app/view/src/pages/Payment.jsx
+++ main/app/view/src/pages/Payment.jsx
@@ -3,40 +3,75 @@ import { Html5QrcodeScanner } from "html5-qrcode"
 import NavBar from "../components/NavBar"
 
 export default function Payment() {
-  const [scanResault, setScanResault] = useState(null)
+  const [scanResult, setScanResult] = useState(null)
+  const [activeTab, setActiveTab] = useState("qr")
+  const [shouldRenderScanner, setShouldRenderScanner] = useState(true)
 
   useEffect(() => {
-    const scanner = new Html5QrcodeScanner("reader", {
-      qrbox: {
-        width: "250px",
-        height: "300px",
-      },
-      fps: 10,
-    })
+    if (shouldRenderScanner) {
+      const scanner = new Html5QrcodeScanner("reader", {
+        qrbox: {
+          width: "250px",
+          height: "300px",
+        },
+        fps: 10,
+      })
 
-    scanner.render(success, error)
+      scanner.render(onSuccess, onError)
 
-    function success(resault) {
-      scanner.clear()
-      setScanResault(resault)
+      return () => {
+        scanner.clear()
+      }
     }
-    function error(err) {
-      console.warn(err)
-    }    
-  }, [])
+  }, [shouldRenderScanner])
+
+  const onSuccess = (result) => {
+    setScanResult(result)
+    setShouldRenderScanner(false)
+  }
+
+  const onError = (error) => {
+    console.warn(error)
+  }
 
   return (
     <>
       <NavBar />
-      {scanResault ? (
-        <div>
-          Success
-          <a href={scanResault} target="_blank" rel="noreferrer">
-            {scanResault}
-          </a>
-        </div>
+
+      <div className="flex-center absolute bottom-32 left-1/2 w-full -translate-x-1/2 transform">
+        <button
+          className={`mt-2 rounded-r-xl border border-gray-500 px-2 py-1 ${activeTab === "nfc" && "bg-primary"}`}
+          onClick={() => {
+            setActiveTab("nfc")
+            setShouldRenderScanner(false)
+          }}
+        >
+          پرداخت با nfc
+        </button>
+        <button
+          className={`mt-2 rounded-l-xl border border-gray-500 px-2 py-1 ${activeTab === "qr" && "bg-primary"}`}
+          onClick={() => {
+            setActiveTab("qr")
+            setShouldRenderScanner(true)
+          }}
+        >
+          پرداخت با qr code
+        </button>
+      </div>
+
+      {activeTab === "qr" ? (
+        scanResult ? (
+          <div>
+            Success
+            <a href={scanResult} target="_blank" rel="noreferrer">
+              {scanResult}
+            </a>
+          </div>
+        ) : (
+          <div id="reader"></div>
+        )
       ) : (
-        <div id="reader"></div>
+        <h1>i am gonna scan for nfc</h1>
       )}
     </>
   )
