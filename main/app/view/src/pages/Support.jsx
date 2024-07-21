c2b1f54691e8923572aeff0ee1f5b2261983cfff
Mahdi <mahdimey17@gmail.com>
2024-07-21 18:31:11 +0330

    add go to user phone app to connect to support person

--- main/app/view/src/pages/Support.jsx
+++ main/app/view/src/pages/Support.jsx
@@ -23,9 +23,11 @@ export default function Support() {
         <BiTimer />
         <button>گزارش اشتباه در زمان اعلامی</button>
       </div>
-      <div className="bg-slate-50 m-auto mt-8 flex w-4/5 cursor-pointer flex-row items-center gap-3 rounded-full border-b-2 border-gray-800 px-3 py-2">
-        <BiSupport />
-        <button>تماس با پشتیبان </button>
+      <div className="bg-slate-50 m-auto mt-8 flex w-4/5 cursor-pointer rounded-full border-b-2 border-gray-800 px-3 py-2">
+        <a href="tel:+989925178741" className="flex flex-row items-center gap-3">
+          <BiSupport />
+          <button>تماس با پشتیبان </button>
+        </a>
       </div>
     </div>
   )
