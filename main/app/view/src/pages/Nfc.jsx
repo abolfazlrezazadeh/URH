dfb20f011b0f9b0fb0ec601fa69a1662c64f781a
Mahdi <mahdimey17@gmail.com>
2024-07-24 23:40:48 +0330

    add button to start scanning and provided user feedback

--- main/app/view/src/pages/Nfc.jsx
+++ main/app/view/src/pages/Nfc.jsx
@@ -5,23 +5,25 @@ import NavBar from "../components/NavBar"
 export default function NFCReader() {
   const [reading, setReading] = useState(false)
   const [tagData, setTagData] = useState(null)
+  const [scanFeedback, setScanFeedback] = useState("")
 
   const navigate = useNavigate()
 
   useEffect(() => {
     // Check if the device is a mobile device
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-    document.write(isMobile)
 
     if (isMobile && "NDEFReader" in window) {
       const ndef = new NDEFReader()
 
       ndef.onreading = (event) => {
         setTagData(event.message)
+        setScanFeedback("Scan complete")
       }
 
       ndef.onerror = (error) => {
-        console.log("Error reading NFC tag:", error)
+        document.write("Error reading NFC tag:", error)
+        setScanFeedback("Error reading NFC tag")
       }
 
       return () => {
@@ -30,8 +32,7 @@ export default function NFCReader() {
       }
     } else {
       alert("NFCReader is not supported on this device.")
-      navigate('/payment')
-
+      navigate("/payment")
     }
   }, [])
 
@@ -39,18 +40,24 @@ export default function NFCReader() {
     if (!reading) {
       ndef.scan()
       setReading(true)
+      setScanFeedback("Scanning...")
     } else {
       // Stop scanning
       ndef.stop()
       setReading(false)
       setTagData(null)
+      setScanFeedback("Scan stopped")
     }
   }
 
   return (
     <>
-    <NavBar />
+      <NavBar />
       <h1>NFC Reader</h1>
+      <button onClick={handleScan}>
+        {reading ? "Stop Scan" : "Start Scan"}
+      </button>
+      <p>{scanFeedback}</p>
       {tagData && (
         <div>
           <h2>Tag Data:</h2>
