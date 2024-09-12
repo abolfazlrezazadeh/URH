ccdc5bb278e1f5910af9429168b951e749b0a8f8
Mahdi <mahdimey17@gmail.com>
2024-09-12 21:41:19 +0330

    add new methods to write nfc tag data

--- main/app/view/src/pages/Ndef.jsx
+++ main/app/view/src/pages/Ndef.jsx
@@ -1,85 +1,83 @@
 import { useEffect, useState } from "react"
-import NfcConsole from "../components/NfcConsole"
+// import NfcConsole from "../components/NfcConsole"
+import nfcImg from "/assets/nfc.jpg"
 
 export default function Ndef() {
   const [feed, setFeed] = useState([])
+  const [nfcValue, setNfcValue] = useState('')
   const [buttonText, setButtonText] = useState("شروع اسکن")
-  const [attemptCount, setAttemptCount] = useState(0)
+  // const [attemptCount, setAttemptCount] = useState(0)
 
   useEffect(() => {
     if (!("NDEFReader" in window)) {
-      alert("NFC is not supported on your device")
+      console.log(" گوشی شما از این تکنولوژی پشتیبانی نمیکند")
     }
   }, [])
 
-  const nfcScan = async () => {
+  const scanTheNfc = async () => {
     try {
       const reader = new NDEFReader()
       await reader.scan()
       setButtonText("در حال اسکن")
-      setAttemptCount((prev) => prev + 1) // Increment the attempt counter
+      // setAttemptCount((prev) => prev + 1) // Increment the attempt counter
 
       reader.addEventListener("error", () => {
-        setFeed((prevFeed) => [
-          ...prevFeed,
-          `Argh! Cannot read data from the NFC tag. Try a different one? Attempt: ${attemptCount + 1}`,
-        ])
+        setFeed('ارور هنگام پرداخت',
+        )
       })
 
-      reader.addEventListener("reading", async ({ message, serialNumber }) => {
-        setFeed((prevFeed) => [`Serial Number: ${serialNumber}`, ...prevFeed])
+       reader.addEventListener("reading", ({ message, serialNumber }) => {
+         setNfcValue(serialNumber)
+       })
 
-        if (attemptCount >= 1) {
-          // Only write data after the first attempt
-          const record = message.records[0]
-          const textDecoder = new TextDecoder(record.encoding)
-          const dynamicData = textDecoder.decode(record.data) // Read dynamic NFC data
-
-          // Writing the dynamic data back to NFC
-          console.log("User clicked write button")
-          try {
-            const ndef = new NDEFReader()
-            await ndef.write({
-              records: [
-                {
-                  recordType: "text",
-                  data: dynamicData, // Use dynamic data read from NFC
-                  language: "en", // Specify the language code
-                },
-              ],
-            })
-            setFeed((prevFeed) => [
-              `Successfully wrote '${dynamicData}' to the NFC tag!`,
-              ...prevFeed,
-            ])
-          } catch (error) {
-            console.error("Write Error: ", error)
-            setFeed((prevFeed) => [
-              "Argh! Failed to write to the NFC tag: " + error.message,
-              ...prevFeed,
-            ])
-          }
-        }
-      })
     } catch (error) {
-      setFeed((prevFeed) => [
-        ...prevFeed,
-        `Argh! ${error.message || error}. Attempt: ${attemptCount + 1}`,
-      ])
-    } finally {
-      setButtonText("گوتولده")
+      setFeed("ارور هنگام پرداخت")
     }
+  }
+
+  async function payTheFare() {
+    try {
+      const writer = new NDEFReader()
+      await writer.write({
+        records: [
+          {
+            recordType: "text",
+            data: '4c:18:62:d2',
+            encoding: "utf-8", // Use proper encoding depending on your data
+          },
+        ],
+      })
+      setFeed(`اتمام پرداخت`)
+    } catch (error) {
+      setFeed("ارور هنگام پرداخت")
+    }
+
   }
 
   return (
     <>
-      <NfcConsole feed={feed} />
-      <button
-        onClick={nfcScan}
-        className="mb-5 mt-5 rounded-lg border-none bg-[#ffc86b] px-10 py-3 text-base font-semibold text-black"
-      >
-        {buttonText}
-      </button>
+      <h1 className="my-10 text-center text-xl">
+        گوشی خود را به دستگاه نزدیک کنید.
+      </h1>
+      {/* <NfcConsole feed={feed} /> */}
+      <img src={nfcImg} alt="nfc-image" className="my-16" />
+      <div className="flex-center gap-4">
+        {/* <button
+          onClick={scanTheNfc}
+          className="rounded-lg border border-gray-300 bg-[#ffc86b] px-10 py-3 text-base font-semibold text-black"
+        >
+          {buttonText}
+        </button> */}
+        <button
+          onClick={payTheFare}
+          className="rounded-lg border border-gray-300 bg-cyan-300 px-10 py-3 text-base font-semibold text-black"
+        >
+          پرداخت
+        </button>
+      </div>
+      <p className="mt-5 text-center text-lg">
+        {feed}
+      </p>
     </>
   )
 }
--- main/app/view/src/pages/NfcR.jsx
+++ main/app/view/src/pages/NfcR.jsx
@@ -1,34 +1,79 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+// import NfcConsole from "../components/NfcConsole"
+import nfcImg from "/assets/nfc.jpg"
 
-export default function NfcR() {
-  const [feedback, setFeedback] = useState("")
+export default function Ndef() {
+  const [feed, setFeed] = useState([])
+  const [nfcValue, setNfcValue] = useState("")
+  const [buttonText, setButtonText] = useState("شروع اسکن")
+  // const [attemptCount, setAttemptCount] = useState(0)
 
-  const ScanNfc = async () => {
-    setFeedback("clicked scan button")
+  useEffect(() => {
+    if (!("NDEFReader" in window)) {
+      alert('گوشین سات خروز آل')
+    }
+  }, [])
+
+  const scanTheNfc = async () => {
     try {
-      // eslint-disable-next-line no-undef
-      const ndef = new NDEFReader()
-      await ndef.scan()
-      setFeedback("> Scan started")
+      const reader = new NDEFReader()
+      await reader.scan()
+      setButtonText("در حال اسکن")
+      // setAttemptCount((prev) => prev + 1) // Increment the attempt counter
 
-      ndef.addEventListener("readingerror", () => {
-        setFeedback("Argh! Cannot read data from the NFC tag. Try another one?")
+      reader.addEventListener("error", () => {
+        setFeed("ارور هنگام پرداخت")
       })
 
-      ndef.addEventListener("reading", ({ message, serialNumber }) => {
-        setFeedback(
-          `> Serial Number: ${serialNumber} \n\n > Records: (${message.records.length}) \n\n ${JSON.stringify(message)}`,
-        )
+      reader.addEventListener("reading", ({ message, serialNumber }) => {
+        setNfcValue(serialNumber)
       })
     } catch (error) {
-      setFeedback("Argh!" + error)
+      setFeed("ارور هنگام پرداخت")
+    }
+  }
+
+  async function payTheFare() {
+    try {
+      const writer = new NDEFReader()
+      setFeed('باشلاده')
+      await writer.write({
+        records: [
+          {
+            recordType: "text",
+            data: "4c:18:62:d2",
+            encoding: "utf-8", // Use proper encoding depending on your data
+          },
+        ],
+      })
+      setFeed(`گوتولده سیهدیره بیلسن`)
+    } catch (error) {
+      setFeed('سیکیم بو دنیانین ایچین گنه ارور ورده')
     }
   }
 
   return (
     <>
-      <button onClick={ScanNfc} className="py-4 px-6 bg-blue-200 mt-40 m-auto block rounded-md mb-5">Scan Nfc</button>
-          <p className="text-center">{feedback}</p>
+      <h1 className="my-10 text-center text-xl">
+        گوشی خود را به دستگاه نزدیک کنید.
+      </h1>
+      {/* <NfcConsole feed={feed} /> */}
+      <img src={nfcImg} alt="nfc-image" className="my-16" />
+      <div className="flex-center gap-4">
+        {/* <button
+          onClick={scanTheNfc}
+          className="rounded-lg border border-gray-300 bg-[#ffc86b] px-10 py-3 text-base font-semibold text-black"
+        >
+          {buttonText}
+        </button> */}
+        <button
+          onClick={payTheFare}
+          className="rounded-lg border border-gray-300 bg-blue-300 px-10 py-3 text-base font-semibold text-black"
+        >
+          پرداخت
+        </button>
+      </div>
+      <p className="mt-5 text-center text-lg">{feed}</p>
     </>
   )
 }
